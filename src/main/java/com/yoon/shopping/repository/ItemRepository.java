0959454c462fdd06a5d116425e36b517334cda8c
newcodingtest <pulpul8282@naver.com>
2022-08-05 16:30:25 +0900

    Feat: "상품 저장 관련 쿼리메소드 구현"
    테스트 코드 작성

--- src/main/java/com/yoon/shopping/repository/ItemRepository.java
+++ src/main/java/com/yoon/shopping/repository/ItemRepository.java
@@ -3,5 +3,15 @@ package com.yoon.shopping.repository;
 import com.yoon.shopping.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item,Long> {
+
+    List<Item> findByItemNm(String itemNm);
+
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    List<Item> findByPriceLessThan(Integer price);
+
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
 }

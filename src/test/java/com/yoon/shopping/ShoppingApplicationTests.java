a536387a4ff1af76804be20b3372494323dc0ac6
newcodingtest <pulpul8282@naver.com>
2022-08-07 14:12:24 +0900

    Initial commit

---
+++ src/test/java/com/yoon/shopping/ShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoon.shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

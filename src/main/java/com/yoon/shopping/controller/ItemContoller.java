a91d54597bea055146c92cabee3e703ffafabd72
newcodingtest <pulpul8282@naver.com>
2022-08-22 00:01:08 +0900

    Feat: "상품 등록 구현중..."

--- src/main/java/com/yoon/shopping/controller/ItemContoller.java
+++ src/main/java/com/yoon/shopping/controller/ItemContoller.java
@@ -1,13 +1,16 @@
 package com.yoon.shopping.controller;
 
+import com.yoon.shopping.dto.ItemFormDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ItemContoller {
 
     @GetMapping(value = "/admin/item/new")
-    public String itemForm(){
+    public String itemForm(Model model){
+        model.addAttribute("itemFormDto", new ItemFormDto());
         return "/item/itemForm";
     }
 }

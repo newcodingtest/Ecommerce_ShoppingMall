b8ba0afd956b24f6494ded595a9180a501011194
yjy <yjy>
2022-09-05 09:52:50 +0900

    test

---
+++ src/main/java/com/yoon/shopping/exception/OutOfStockException.java
@@ -0,0 +1,9 @@
+package com.yoon.shopping.exception;
+
+public class OutOfStockException extends RuntimeException{
+
+    //상품 주문 수량 > 상품 재고 수량 일때 발생
+    public OutOfStockException(String message){
+        super(message);
+    }
+}
